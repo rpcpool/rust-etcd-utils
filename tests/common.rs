3c6f5d8f1fb5a6366d8e30f6bb7cbf519016be2a
lvboudre <louis-vincent@triton.one>
2024-11-29 16:03:22 -0500

    added better lease revoking detection (#9)

--- tests/common.rs
+++ tests/common.rs
@@ -1,3 +1,7 @@
+use std::io::{self, IsTerminal};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
 pub async fn get_etcd_client() -> etcd_client::Client {
     let url = option_env!("ETCD_TEST_URL").unwrap_or("http://localhost:2379");
     etcd_client::Client::connect([url], None)
@@ -5,6 +9,7 @@ pub async fn get_etcd_client() -> etcd_client::Client {
         .expect("failed to connect to etcd")
 }
 
+#[allow(dead_code)]
 pub fn random_str(len: usize) -> String {
     use rand::{distributions::Alphanumeric, thread_rng, Rng};
     let mut rng = thread_rng();
@@ -13,4 +18,21 @@ pub fn random_str(len: usize) -> String {
         .take(len)
         .map(char::from)
         .collect()
+}
+
+#[allow(dead_code)]
+pub fn setup_tracing() {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+    let subscriber = tracing_subscriber::registry().with(env_filter);
+
+    let is_atty = io::stdout().is_terminal() && io::stderr().is_terminal();
+    let io_layer = tracing_subscriber::fmt::layer()
+        .with_line_number(true)
+        .with_ansi(is_atty);
+    subscriber
+        .with(io_layer)
+        .try_init()
+        .expect("failed to setup tracing");
 }
---
+++ tests/test_lease.rs
@@ -0,0 +1,122 @@
+use std::time::Duration;
+
+use rust_etcd_utils::lease::ManagedLeaseFactory;
+
+mod common;
+
+#[tokio::test]
+async fn it_should_automatically_refresh_lease() {
+    let etcd = common::get_etcd_client().await;
+    let managed_lease_factory = ManagedLeaseFactory::new(etcd.clone());
+
+    let managed_lease = managed_lease_factory
+        .new_lease(Duration::from_secs(2), None)
+        .await
+        .expect("failed to create lease");
+    let lease_expire_notify = managed_lease.get_lease_expire_notify();
+
+    tokio::select! {
+        _ = lease_expire_notify.recv() => {
+            panic!("lease expired before the lease duration");
+        }
+        _ = tokio::time::sleep(Duration::from_secs(6)) => {}
+    }
+    assert!(managed_lease.is_alive().await.expect("lease is not alive"));
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn it_should_limit_the_amount_of_lease_refresh() {
+    let etcd = common::get_etcd_client().await;
+    let managed_lease_factory = ManagedLeaseFactory::new(etcd.clone());
+
+    let lease_created_at = std::time::Instant::now();
+    let ttl = Duration::from_secs(2);
+    let refresh_interval = Duration::from_secs(1);
+    let refresh_limit = 3;
+    let managed_lease = managed_lease_factory
+        .new_lease_with_auto_refresh_limit(ttl, Some(refresh_interval), Some(refresh_limit))
+        .await
+        .expect("failed to create lease");
+
+    let estimated_lease_duration = refresh_interval * (refresh_limit as u32);
+
+    let lease_expired_notify = managed_lease.get_lease_expire_notify();
+    tokio::select! {
+        _ = lease_expired_notify.recv() => {
+            let elapsed = lease_created_at.elapsed();
+            assert!(elapsed >= estimated_lease_duration, "lease expired before the auto refresh limit");
+        }
+        _ = tokio::time::sleep(estimated_lease_duration * 2) => {
+            assert!(!managed_lease.is_alive().await.expect("failed to get lease alive status"));
+
+        }
+    }
+
+    assert!(managed_lease.is_alive().await.expect("lease is not alive"));
+}
+
+#[tokio::test]
+async fn test_lease_expire_notify_on_drop() {
+    let etcd = common::get_etcd_client().await;
+    let managed_lease_factory = ManagedLeaseFactory::new(etcd.clone());
+
+    let managed_lease = managed_lease_factory
+        .new_lease(Duration::from_secs(2), None)
+        .await
+        .expect("failed to create lease");
+    let lease_expire_notify = managed_lease.get_lease_expire_notify();
+    let lease_expire_notify_clone = lease_expire_notify.clone();
+    let weak_lease_expire_notify = managed_lease.get_weak().get_lease_expire_notify();
+
+    drop(managed_lease);
+
+    let _ = weak_lease_expire_notify.recv().await;
+    let _ = lease_expire_notify_clone.recv().await;
+    let _ = lease_expire_notify.recv().await;
+}
+
+#[tokio::test]
+async fn test_lease_expire_notify_when_etcd_revoke() {
+    let etcd = common::get_etcd_client().await;
+    let managed_lease_factory = ManagedLeaseFactory::new(etcd.clone());
+
+    let managed_lease = managed_lease_factory
+        .new_lease(Duration::from_secs(2), None)
+        .await
+        .expect("failed to create lease");
+    let lease_expire_notify = managed_lease.get_lease_expire_notify();
+    let lease_expire_notify_clone = lease_expire_notify.clone();
+    let weak_lease_expire_notify = managed_lease.get_weak().get_lease_expire_notify();
+
+    etcd.lease_client()
+        .revoke(managed_lease.lease_id())
+        .await
+        .expect("failed to revoke lease");
+
+    assert!(!managed_lease.is_alive().await.expect("lease is not alive"));
+    let _ = weak_lease_expire_notify.recv().await;
+    let _ = lease_expire_notify_clone.recv().await;
+    let _ = lease_expire_notify.recv().await;
+}
+
+#[tokio::test]
+async fn lease_revoke_notify_should_return_immediately_if_created_after_delete() {
+    let etcd = common::get_etcd_client().await;
+    let managed_lease_factory = ManagedLeaseFactory::new(etcd.clone());
+
+    let managed_lease = managed_lease_factory
+        .new_lease(Duration::from_secs(2), None)
+        .await
+        .expect("failed to create lease");
+
+    // Revoke before the lease notify is created
+    etcd.lease_client()
+        .revoke(managed_lease.lease_id())
+        .await
+        .expect("failed to revoke lease");
+
+    let lease_expire_notify = managed_lease.get_lease_expire_notify();
+
+    assert!(!managed_lease.is_alive().await.expect("lease is not alive"));
+    let _ = lease_expire_notify.recv().await;
+}
